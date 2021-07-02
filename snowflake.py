3ce3c7e8482951a4f874632f1cdc19013e4b2858
kungyu <hismayfly@gmail.com>
2021-07-03 01:47:32 +0900

    revision

--- snowflake.py
+++ snowflake.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import datetime
 
 
@@ -5,28 +6,27 @@ class Generator():
     TWITTER_EPOCH = 128883497457 // 1000
     SEQUENCE_NUM = 0
 
-    def __init__(self, datacenter=1, machine=1):
+    def __init__(self, datacenter: int=1, machine: int=1):
         self.datacenter = datacenter
         self.machine = machine
 
-    def _fill_zeros(self, binary, num):
+    def _fill_zeros(self, binary: str, num: int):
         while len(binary) < num:
-            num = '0' + num
+            binary = '0' + binary
 
-        return num
+        return binary
 
     def _get_timestamp(self):
-        epoch = self._get_timestamp()
-        now = int(datetime.now() * 1000)
+        now = int(datetime.now().timestamp() * 1000)
 
-        diff = now - TWITTER_EPOCH
+        diff = now - self.TWITTER_EPOCH
         diff_in_binary = bin(diff)[2:]
         filled = self._fill_zeros(diff_in_binary, 41)
 
         return filled
 
     def _get_datacenter(self):
-        dc_binary = bin(self.datacneter)[2:]
+        dc_binary = bin(self.datacenter)[2:]
         filled = self._fill_zeros(dc_binary, 5)
 
         return filled
@@ -37,12 +37,47 @@ class Generator():
 
         return filled
 
+    def _get_sequence(self):
+        filled = self._fill_zeros(str(self.SEQUENCE_NUM), 5)
+
+        return filled
+
     def generate(self):
         ts = self._get_timestamp()
         dc = self._get_datacenter()
         m = self._get_machine()
+        s = self._get_sequence()
+        
+        unique_id = f"0-{ts}-{dc}-{m}-{s}"
+        self.SEQUENCE_NUM += 1
 
-        return f"0-{ts}-{dc}-{m}-"
+        return unique_id
+
+
+def main(datacenter=None, machine=None):
+    gen = Generator()
+
+    running = 1
+    start = datetime.now()
+    while running:
+        num = int(input("How many IDs?(default=1, exit=0): "))
+        
+        if num:
+            for _ in range(num):
+                print(gen.generate())
+        elif num == 0:
+            running = 0
+        else:
+            print(gen.generate())
+
+        if (datetime.now() - start).total_seconds() > 3:
+            gen.SEQUENCE_NUM = 0
+            start = datetime.now()
+
 
 if __name__ == "__main__":
-    gen = Generator()
+    if len(sys.argv) >= 3:
+        datacenter, machine = sys.argv[1], sys.argv[2]
+        main(datacenter, macine)
+    else:
+        main()
